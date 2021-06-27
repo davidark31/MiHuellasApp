e1a7e25aaf0518efd7ed4fcd2f0894c2e01bab9d
davidark31 <d.rivas05@ufromail.cl>
2021-06-27 16:57:26 -0400

    0.01

---
+++ app/src/main/java/com/example/mihuellasapp/FormularioLogin.java
@@ -0,0 +1,14 @@
+package com.example.mihuellasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_login);
+    }
+}
