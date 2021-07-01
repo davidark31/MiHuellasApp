82d3a06d888678ec8b3c76fc4302d652f70c4c2f
davidark31 <d.rivas05@ufromail.cl>
2021-07-01 00:55:42 -0400

    0.023 iconos

---
+++ app/src/main/java/com/example/mihuellasapp/Mascota.java
@@ -0,0 +1,134 @@
+package com.example.mihuellasapp;
+
+public class Mascota {
+
+    private String id;
+    private String nombre;
+    private String animal;
+    private String sexo;
+    private String color;
+    private String color2;
+    private String raza;
+    private String tamaño;
+    private int edad;
+    private String descripcion;
+    private String estado;
+    private String IdDueño;
+
+    public Mascota(String nombre, String animal, String sexo, String color, String raza, String tamaño, int edad, String descripcion, String estado, String idDueño) {
+        this.nombre = nombre;
+        this.animal = animal;
+        this.sexo = sexo;
+        this.color = color;
+        this.raza = raza;
+        this.tamaño = tamaño;
+        this.edad = edad;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.IdDueño = idDueño;
+    }
+
+
+    public void Mascota() {
+        estado = "En Casa";
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor2() {
+        return color2;
+    }
+
+    public void setColor2(String color2) {
+        this.color2 = color2;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getIdDueño() {
+        return IdDueño;
+    }
+
+    public void setIdDueño(String idDueño) {
+        IdDueño = idDueño;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/mihuellasapp/Registrar_MascotaActivity.java
@@ -0,0 +1,20 @@
+package com.example.mihuellasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Registrar_MascotaActivity extends AppCompatActivity {
+
+    private Spinner spi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_mascota);
+
+
+    }
+}
