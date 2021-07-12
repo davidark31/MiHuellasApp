7200cb1831ee87efba367913c664767b98b80d79
davidark31 <d.rivas05@ufromail.cl>
2021-07-11 20:04:24 -0400

    splash art

---
+++ app/src/main/java/com/example/mihuellasapp/MascotaPerdidaActivity.java
@@ -0,0 +1,14 @@
+package com.example.mihuellasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MascotaPerdidaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascota_perdida);
+    }
+}
